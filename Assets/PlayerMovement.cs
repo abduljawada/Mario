c9c92a31075c92e4bda1d376935f753332cd6649
abduljawada <125431874+abduljawada@users.noreply.github.com>
2023-06-08 11:13:35 +0400

    Upgraded to Unity 2022.3.0f1 and added jump

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,9 +7,12 @@ public class PlayerMovement : MonoBehaviour
     private SpriteRenderer SpriteRenderer => GetComponent<SpriteRenderer>();
 
     private float _moveDir;
+    private float _jumpLatencyTime;
 
     [SerializeField] private float acceleration = 3f;
     [SerializeField] private float maxSpeed = 5f;
+    [SerializeField] private float jumpForce = 10f;
+    [SerializeField] private float jumpLatencyStartTime = 0.2f;
     private static readonly int IsWalking = Animator.StringToHash("IsWalking");
     private static readonly int IsDrifting = Animator.StringToHash("IsDrifting");
 
@@ -17,8 +20,20 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         _moveDir = Input.GetAxisRaw("Horizontal");
+
+        _jumpLatencyTime -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) _jumpLatencyTime = jumpLatencyStartTime;
+
+        if (_jumpLatencyTime > 0 && IsGrounded())
+        {
+            Rigidbody2D.velocity *= Vector2.right;
+            Rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
         Animator.SetBool(IsWalking, _moveDir != 0);
         Animator.SetBool(IsDrifting, (_moveDir * Rigidbody2D.velocity.x) < 0);
+        
         if (_moveDir != 0)
         {
             SpriteRenderer.flipX = _moveDir < 0;
@@ -27,6 +42,21 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Rigidbody2D.velocity = new Vector2(Mathf.Lerp(Rigidbody2D.velocity.x, _moveDir * maxSpeed, Time.deltaTime * acceleration), Rigidbody2D.velocity.y);
+        //Rigidbody2D.velocity = new Vector2(_moveDir * maxSpeed, 0);
+
+        float _moveVelocityX = Mathf.Lerp(Rigidbody2D.velocity.x, _moveDir * maxSpeed, Time.deltaTime * acceleration);
+
+        Rigidbody2D.velocity = new Vector2(_moveVelocityX, Rigidbody2D.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 0.6f), 0.05f);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(new Vector2(transform.position.x, transform.position.y - 0.6f), 0.05f);
     }
 }
